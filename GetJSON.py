ae87f670e57712c9a58b5ecc63bb4838fb3fe479
Alexandra Sciocchetti <76969118+scioccha@users.noreply.github.com>
2022-03-07 12:40:50 -0800

    Cleaned up regex sections 1.0

--- GetJSON.py
+++ GetJSON.py
@@ -1,6 +1,9 @@
+# Programmer: Alexandra Sciocchetti
+# Course: CS 361
+# Description: JSON parsing/data cleaning microservice that takes USGS GeoJSON data and
+#               reformats the data in a way that is easy to query from a database
 
-#https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/significant_month.geojson
-
+#Load Packages:
 import json
 import urllib.request as request
 import pandas as pd
@@ -8,12 +11,13 @@ import datetime
 import regex as re
 import unidecode as unidecode
 
-with request.urlopen("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1970-01-01&endtime=2022-02-20&minmagnitude=6.5") as response:
+#Get data from USGS
+with request.urlopen("https://earthquake.usgs.gov/fdsnws/event/1/query?format=geojson&starttime=1970-01-01&endtime=2022-03-07&minmagnitude=6.5") as response:
     source = response.read()
     sig_quakes = json.loads(source) #read in data
+    quake_features = sig_quakes["features"] #all relevant data contained within "features" list
 
-quake_features = sig_quakes["features"] #all relevant data contained within "features" list
-
+#Define lists to store relevant earthquake data
 list_url = []
 list_mag = []
 list_place = []
@@ -25,69 +29,60 @@ latitude_list = []
 longitude_list = []
 depth_list = []
 
-def add_feature_data(source):
-    for i in source:
-        magnitude = [i][0]["properties"]["mag"] #second type of related data: earthquake properties
-        shaking_intensity = [i][0]["properties"]["mmi"]
-        place = [i][0]["properties"]["place"]
-        time = [i][0]["properties"]["time"]
-        url = [i][0]["properties"]["url"]
-        sig = [i][0]["properties"]["sig"]
-        alert = [i][0]["properties"]["tsunami"]
-        list_url.append(url) #add all defined variables to lists
-        list_mag.append(magnitude)
-        list_place.append(place)
-        list_datetime.append(time)
-        list_intensity.append(shaking_intensity)
-        list_sig.append(sig)
-        list_tsunami.append(alert)
-
-def add_geometry_data(source):
-    for i in source:
-        longitude = [i][0]["geometry"]["coordinates"][0]
-        latitude = [i][0]["geometry"]["coordinates"][1]
-        depth = [i][0]["geometry"]["coordinates"][2]
-        latitude_list.append(latitude)
-        longitude_list.append(longitude)
-        depth_list.append(depth)
-
-add_feature_data(quake_features) #run the function for feature list
-add_geometry_data(quake_features)
-
+#city and region list will be extracted from "place" data
 city_list=[]
 region_list=[]
-for place in list_place:
-    try:
-        x= place.split('of',) # location is always after "of" in the string
-        y = x[-1]
-        z = y.split(',',)#city and region are separated by a comma
-        city_regex = re.sub(r"[^\w ]",' ', z[0]) #remove all special characters
-        city_final = str.lstrip(city_regex) #remove beginning space
-        city_final2 = unidecode.unidecode(city_final)
-        region_regex = re.sub(r"[^\w ]",' ', z[-1]) #do the same for region
-        region_final = str.lstrip(region_regex)
-        city_list.append(str.rstrip(city_final2)) #remove any end spaces and add to list
-        region_list.append(str.rstrip(region_final))
-    except AttributeError:
-        city = str(place)
-        city_regex = re.sub(r"[^\w ]", ' ', city)  # remove all special characters
-        city_final = str.lstrip(city_regex)
-        city_final2 = unidecode.unidecode(city_final)
-        city_list.append(str(city_final2))
 
-        region_list.append(str(place))
+def add_feature_data(source):
+    """
+    Takes as a parameter a loaded USGS GeoJSON file and adds relevant elements to earthquake lists. Elements
+    added are earthquake event url, magnitude, place, time, intensity (recorded as mmi), significance, and tsunami
+    indicator. List of data formats and descriptions can be found here:
+    https://earthquake.usgs.gov/earthquakes/feed/v1.0/geojson.php
+    """
+    for i in source:
+        list_url.append([i][0]["properties"]["url"])
+        list_mag.append([i][0]["properties"]["mag"])
+        list_place.append([i][0]["properties"]["place"])
+        list_datetime.append([i][0]["properties"]["time"])
+        list_intensity.append([i][0]["properties"]["mmi"])
+        list_sig.append([i][0]["properties"]["sig"])
+        list_tsunami.append([i][0]["properties"]["tsunami"])
+
+def add_geometry_data(source):
+    """
+    Similar to add_feature_data, takes a loaded GeoJSON file and extracts earthquake latitude, longitude, and
+    depth which are stored as a three element tuple within the earthquake geometry dictionary. Elements are added
+    to relevant lists.
+    """
+    for i in source:
+        latitude_list.append([i][0]["geometry"]["coordinates"][1])
+        longitude_list.append([i][0]["geometry"]["coordinates"][0])
+        depth_list.append([i][0]["geometry"]["coordinates"][2])
+
+def remove_white_space_special_chars(location):
+    no_white_space = str.lstrip(re.sub(r"[^\w ]",' ', location))
+    remove_region = re.sub(r"region",'',no_white_space)
+    return unidecode.unidecode(remove_region)
 
 
+def extract_city_and_region(place_list):
+    for place in place_list:
+        try:
+            y= place.split('of',)[-1] # location is always after "of" in the string
+            z = y.split(',',) #city and region are separated by a comma
+            city,region = remove_white_space_special_chars(z[0]), remove_white_space_special_chars(z[-1])
+            city_list.append(city)
+            region_list.append(region)
+        except AttributeError:
+            city = remove_white_space_special_chars(str(place))
+            city_list.append(city), region_list.append(str(place))
 
-#city_list = [re.sub(r"\s+", '-', file) for file in city_list] #replace spaces with dashes
-#region_list = [re.sub(r"\s+", '-', file) for file in region_list] #replace spaces with dashes
+#Apply feature and geometry data functions to the quake features data:
+add_feature_data(quake_features)
+add_geometry_data(quake_features)
+extract_city_and_region(list_place)
 
-region_list = [re.sub(r"region", '', file) for file in region_list] #fix naming issues in regions
-region_list = [re.sub(r"(\w+) *-Peru", 'Peru', file) for file in region_list]
-region_list = [re.sub(r"(\w+) *-Alaska", 'Alaska', file) for file in region_list]
-
-city_list = [re.sub(r"region", '', file) for file in city_list]
-city_list = [str(i or None) for i in city_list] #replace empty city names with None
 
 
 date_list = []
@@ -95,11 +90,10 @@ for t in list_datetime:
     date = datetime.datetime.fromtimestamp(t/1000.0, tz=datetime.timezone.utc)
     full_date = date.strftime('%Y-%m-%d %H:%M:%S')
     date = re.findall(r'\d{4}-\d{2}-\d{2}', full_date) #separate date from time
-    #date_list.append(full_date)
     date_list.append(date)
 
 date_list = [i[0] for i in date_list] #remove additional brackets from date list
-#date_list = [re.sub(r"\b0","", file) for file in date_list] #remove leading zeros
+
 
 feature_dict= {'city':city_list, 'region':region_list,'magnitude':list_mag, 'url': list_url, 'place': list_place,
        'date':date_list, 'intensity': list_intensity, 'significance': list_sig, 'tsunami': list_tsunami,
